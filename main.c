d63c0799bebeaf6bffa38c31c2da5dea433bc6a5
NicGobbi <nicolas.gobbi@usp.br>
2020-10-30 20:31:10 -0300

    MVP -> threads start and run around. next step is removing losers

--- main.c
+++ main.c
@@ -4,51 +4,122 @@
 #include <unistd.h>
 
 #define TRACK_QUANT 10
+#define MAX_THREADS_STARTED 5
+#define EMPTY_POSITION_VALUE -1
 
-unsigned int* arrive;
-unsigned int* cont;
-pthread_t* threads;
-unsigned int* thread_ids;
 pthread_mutex_t* locks;
 int** pista;
+int d;
 
+typedef struct
+{
+  pthread_t thread_id;
+  unsigned int runner_id;
+  unsigned int arrive;
+  unsigned int keep_going;
+  unsigned int started;
+
+  unsigned int pista_position;
+  unsigned int pista_line;
+} runner;
+
+runner* runners;
+
+void print_pista()
+  {
+    fprintf(stderr, "\n");
+    for(int i = 0; i < TRACK_QUANT; i++)
+      {
+        for(int j = 0; j < d; j++)
+          {
+            if(pista[j][i] < 10 && pista[j][i] >= 0)
+              fprintf(stderr, "  %d", pista[j][i]);
+            else
+              fprintf(stderr, " %d", pista[j][i]);
+          }
+        fprintf(stderr, "\n");
+      }
+  }
+
+void move_forward(int id)
+  {
+    int current_position = runners[id].pista_position;
+    int current_line = runners[id].pista_line;
+    int possible_next_position = (current_position + 1)%d;
+    if(current_position != 0)
+      {
+        pthread_mutex_lock(&locks[current_position]);
+        pthread_mutex_lock(&locks[possible_next_position]);
+      }
+    else
+      {
+        pthread_mutex_lock(&locks[possible_next_position]);
+        pthread_mutex_lock(&locks[current_position]);
+      }
+    if(pista[possible_next_position][current_line] == EMPTY_POSITION_VALUE)
+      {
+        pista[possible_next_position][current_line] = id;
+        pista[current_position][current_line] = -1;
+        runners[id].pista_position = possible_next_position;
+      }
+    pthread_mutex_unlock(&locks[possible_next_position]);
+    pthread_mutex_unlock(&locks[current_position]);
+  }
 
 void * runner_thread(void * a)
   {
     int temp;
     int* id = (int*)a;
-    fprintf(stderr, "Thread %d started\n", *id);
+    int posi = 0;
+    //fprintf(stderr, "Thread %d started\n", *id);
     while(1)
       {
-        pthread_mutex_lock(locks[posi])
-        pthread_mutex_lock(locks[posi+1])
-        for(int i = 0; i < 500; i++)
+        if(runners[*id].started)
           {
-            temp++;
-            usleep(1);
+            move_forward(*id);
           }
-        arrive[*id] = 1;
-        fprintf(stderr, "Thread %d is finished for this round", *id);
-        while(cont[*id] == 0) usleep(1000);
-        cont[*id] = 0;
+        runners[*id].arrive = 1;
+        //fprintf(stderr, "Thread %d is finished for this round", *id);
+        while(runners[*id].keep_going == 0) usleep(60000);
+        runners[*id].keep_going = 0;
       }
   }
 
 void * coordinator_thread(void * a)
   {
     int buff;
-    int* worker_count = (int*)a;
+    int* runner_count = (int*)a;
+    int started_runners = 0;
+    int alternate_start_position = 0;
     fprintf(stderr, "Thread coordinator started\n");
     while(1)
       {
-        for(int i = 0; i < *worker_count; i++)
+        for(int i = 0; i < *runner_count; i++)
           {
-            while (arrive[i] == 0);
-            arrive[i] = 0;
+            while (runners[i].arrive == 0);
+            runners[i].arrive = 0;
           }
-        fprintf(stderr, "ready for next run?");
-        scanf("%d", &buff);
-        for(int i = 0; i < *worker_count; i++) cont[i] = 1;
+        /* inicio da corrida */
+        int threads_to_start = *runner_count - started_runners;
+        //fprintf(stderr, "Threads to start %d.", threads_to_start);
+        if(threads_to_start > MAX_THREADS_STARTED)
+          threads_to_start = MAX_THREADS_STARTED;
+        if(threads_to_start > 0)
+          {
+            for(int i = 0; i < threads_to_start; i++)
+              {
+                runners[started_runners].started = 1;
+                runners[started_runners].pista_position = 0;
+                runners[started_runners].pista_line = i + alternate_start_position;
+                pista[0][i + alternate_start_position] = runners[started_runners++].runner_id;
+                fprintf(stderr, "%d", i + alternate_start_position);
+              }
+            alternate_start_position = (alternate_start_position + MAX_THREADS_STARTED) % 2*MAX_THREADS_STARTED;
+          }
+        /* debug thread initialization */
+        print_pista();
+        fprintf(stderr, "ready for next run.");
+        for(int i = 0; i < *runner_count; i++) runners[i].keep_going = 1;
       }
   }
 
@@ -57,29 +128,29 @@ int main()
     // initializations
     printf("How many worker threads?");
     int thread_count;
-    int d;
     scanf("%d", &thread_count);
     printf("Track size");
     scanf("%d", &d);
-    threads = malloc(sizeof(pthread_t)*thread_count);
-    arrive = malloc(sizeof(int)*thread_count);
-    cont = malloc(sizeof(int)*thread_count);
-    thread_ids = malloc(sizeof(int)*thread_count);
+    runners = malloc(sizeof(runner)*thread_count);
     locks = malloc(sizeof(pthread_mutex_t)*d);
     pista = malloc(sizeof(int*)*d);
     for(int i = 0; i < d; i++)
       {
         pista[i] = malloc(sizeof(int)*TRACK_QUANT);
         pthread_mutex_init(&locks[i], NULL);
+        for(int j = 0; j < TRACK_QUANT; j++)
+          {
+            pista[i][j] = EMPTY_POSITION_VALUE;
+          }
       }
+    print_pista();
     for(int i = 0; i < thread_count; i++)
       {
-        thread_ids[i] = i;
-        fprintf(stderr, "*i= %p, *tid = %p, i = %d, tid = %d\n", &i,&thread_ids[i], i, thread_ids[i]);
-        pthread_create(&threads[i], NULL, runner_thread, &thread_ids[i]);
+        runners[i].runner_id = i;
+        runners[i].started = 0;
+        pthread_create(&runners[i].thread_id, NULL, runner_thread, &runners[i].runner_id);
       }
     pthread_t coord;
     pthread_create(&coord, NULL, coordinator_thread, &thread_count);
     pthread_join(coord, NULL);
   }
-
